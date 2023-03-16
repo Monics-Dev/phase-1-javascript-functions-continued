5d39d9e41a02a016502150327bdde69e8000a432
Monica <monijerobon@gmail.com>
2023-03-16 16:51:33 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// code your solution here
+
+function saturdayFun (toDo = "roller-skate" ){
+    return `This Saturday, I want to ${toDo}!`;
+}
+function mondayWork (day="go to the office" ){
+    return`This Monday, I will ${day}.`;
+}
+mondayWork();
+mondayWork("work from work");
+
+
+  function wrapAdjective(testing = "*") {
+  return function (mypar = "special") {
+    return `You are ${testing}${mypar}${testing}!`;
+  }
+}
+
+
+
+
